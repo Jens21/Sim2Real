26b52ff38340a7c70683ca9d065cdd5734ce3833
User <jens-beisswenger@gmx.de>
2021-10-03 22:37:16 +0200

    scenarios.py modified

--- ConvertAmira2YCB/scenarios.py
+++ ConvertAmira2YCB/scenarios.py
@@ -27,26 +27,6 @@ class Color():
             img_result[:,:,0:3]=np.where(img_backdrop[:,:]==0, img_result[:,:,0:3], np.multiply(img_rgb,255.0/2**16))
             img_result[:,:,3]=img_backdrop[:,:,0]
 
-            """
-            for y in range(480):
-                for x in range(640):
-                    back=255-img_backdrop[y,x,0]
-
-                    if back!=0:
-                        img_result[y,x,0]=img_rgb[y,x,0]/2**16*255.0
-                        img_result[y,x,1]=img_rgb[y,x,1]/2**16*255.0
-                        img_result[y,x,2]=img_rgb[y,x,2]/2**16*255.0
-                        img_result[y,x,3]=back
-
-            
-            img_backdrop=np.subtract(255,img_backdrop)
-            img_rgb=cv2.cvtColor(img_rgb, cv2.COLOR_RGB2RGBA)
-
-            img_rgb[:,:,3]=img_backdrop
-            img_rgb[:,:,0]=np.where(np.equal(img_backdrop,0),img_backdrop,img_rgb[:,:,0])
-            img_rgb[:,:,1]=np.where(np.equal(img_backdrop,0),img_backdrop,img_rgb[:,:,1])
-            img_rgb[:,:,2]=np.where(np.equal(img_backdrop,0),img_backdrop,img_rgb[:,:,2])
-            """
             return img_result
 
         return None
@@ -65,20 +45,7 @@ class Depth():
 
     def scenario_1(self,file_depth, file_backdrop):  #This scenario makes the background transparent
         if os.path.isfile(file_depth) and os.path.isfile(file_backdrop):
-            img_depth=cv2.imread(file_depth, cv2.IMREAD_UNCHANGED)[:,:,0]
-            img_backdrop=cv2.imread(file_backdrop, cv2.IMREAD_UNCHANGED)[:,:,0]
 
-            img_depth=np.where(np.greater(img_depth,10),0,img_depth)
-            img_depth=np.multiply(img_depth,10000.0)
-
-            img_result=np.zeros((480,640),dtype="uint16")
-            img_backdrop=np.subtract(65535,img_backdrop)
-
-            img_result=np.where(np.equal(img_backdrop,0),img_result,img_depth)
-
-            return img_result
-
-            """
             img_depth=cv2.imread(file_depth, cv2.IMREAD_UNCHANGED)[:,:,0]
             img_backdrop=cv2.imread(file_backdrop, cv2.IMREAD_UNCHANGED)[:,:,0]
 
@@ -91,5 +58,5 @@ class Depth():
             img_depth=np.where(np.equal(img_backdrop,0),0,img_depth)
 
             return img_depth
-            """
+            
         return None
